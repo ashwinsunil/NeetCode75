404dafddb349270fe6707636619e70a12a4d3cb6
Ashwin Kumar <ashwinsunilkumar12@gmail.com>
2025-07-05 16:42:55 +0530

    add: kth Smallest Integer in a BST solution

--- Trees/kthsmallestIntegerInBST.js
+++ Trees/kthsmallestIntegerInBST.js
@@ -5,4 +5,15 @@ class TreeNode {
     this.right = right;
   }
 }
-function kthSmallest(root, k) {}
+function kthSmallest(root, k) {
+  let inorderTraversal = inOrder(root, []);
+  return inorderTraversal[k - 1];
+}
+function inOrder(root, answer) {
+  if (!root) return;
+  inOrder(root.left, answer);
+  answer.push(root.val);
+  inOrder(root.right, answer);
+  return answer;
+}
+console.log(kthSmallest(new TreeNode(3, new TreeNode(2, new TreeNode(1))), 1));
